720fb5e910878b0c9a6634c82599cd3e492c7af2
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-21 17:12:30 +0800

    连接池init

---
+++ day03_datasourceAndTx/src/main/java/com/cc/domain/QueryVo.java
@@ -0,0 +1,14 @@
+package com.cc.domain;
+
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

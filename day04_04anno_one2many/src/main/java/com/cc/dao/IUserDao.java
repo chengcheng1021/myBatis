96f29a4acf85403d1272cf45c878794dd55dc9e3
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-23 08:41:21 +0800

    解决数据库字段和实体类属性不一致问题

---
+++ day04_04anno_one2many/src/main/java/com/cc/dao/IUserDao.java
@@ -0,0 +1,48 @@
+package com.cc.dao;
+
+import com.cc.domain.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * 在 mybatis 中针对，CRUD 一共有4个注解。首字母大写
+ * @SELECT
+ * @INSERT
+ * @UPDATE
+ * @DELETE
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+    @Select("select * from user")
+    @Results(id = "userMap", value = {
+            @Result(id = true, column = "id", property = "userId"),
+            @Result(column = "username", property = "userName"),
+            @Result(column = "address", property = "userAddress"),
+            @Result(column = "sex", property = "userSex"),
+            @Result(column = "birthday", property = "userBirthday"),
+    })
+    List<User> findAll();
+
+    /**
+     * 根据 id 查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id=#{id}")
+    @ResultMap(value = {"userMap"})
+    User findById(Integer userId);
+
+    /**
+     * 根据用户模糊查询
+     * @param username
+     * @return
+     */
+    @Select("select * from user where username like #{username}")
+    @ResultMap("userMap")
+    List<User> findUserByName(String username);
+}
---
+++ day04_04anno_one2many/src/test/java/com/cc/test/AnnotationCRUDTest.java
@@ -0,0 +1,61 @@
+package com.cc.test;
+
+import com.cc.dao.IUserDao;
+import com.cc.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+public class AnnotationCRUDTest {
+
+    private InputStream is;
+    private SqlSessionFactory factory;
+    private SqlSession session;
+    private IUserDao userDao;
+
+    @Before
+    public void init() throws IOException {
+        is = Resources.getResourceAsStream("SqlMapConfig.xml");
+        factory = new SqlSessionFactoryBuilder().build(is);
+        session = factory.openSession();
+        userDao = session.getMapper(IUserDao.class);
+    }
+
+    @After
+    public void destory() throws IOException {
+        session.commit();
+        session.close();
+        is.close();
+    }
+
+    @Test
+    public void testFindAll() {
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testFindOne() {
+        User user = userDao.findById(55);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testFindUserByName() {
+        List<User> users = userDao.findUserByName("%王%");
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}

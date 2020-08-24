3cb722b151e8bb4b0565c257fb281b03c6672368
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-24 15:44:27 +0800

    配置属性优化

---
+++ Mybatis-Study/mybatis-02/src/test/java/com/cc/dao/UserDaoTest.java
@@ -0,0 +1,33 @@
+package com.cc.dao;
+
+import com.cc.pojo.User;
+import com.cc.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class UserDaoTest {
+
+    @Test
+    public void test() {
+        //第一步：获取 sqlSession 对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //第二步：执行SQL
+        //方式一：getMapper
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = userMapper.getUserList();
+
+        //方式二：
+        //List<User> userList = sqlSession.selectList("com.cc.dao.UserDao.getUserList");
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        //关闭 sqlSession
+        sqlSession.close();
+    }
+}

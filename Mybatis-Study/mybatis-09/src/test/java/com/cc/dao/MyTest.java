f019aa75971d45f05f9613db22b347e7a37ab7d2
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-25 18:03:23 +0800

    二级缓存

--- Mybatis-Study/mybatis-09/src/test/java/com/cc/dao/MyTest.java
+++ Mybatis-Study/mybatis-09/src/test/java/com/cc/dao/MyTest.java
@@ -21,5 +21,27 @@ public class MyTest {
 
         User user2 = mapper.queryUserById(41);
         System.out.println(user2);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test2() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        SqlSession sqlSession2 = MybatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        UserMapper mapper2 = sqlSession2.getMapper(UserMapper.class);
+
+        User user = mapper.queryUserById(41);
+        System.out.println(user);
+
+        User user2 = mapper2.queryUserById(41);
+        System.out.println(user2);
+
+        System.out.println(user == user2);
+
+        sqlSession.close();
+        sqlSession2.close();
     }
 }
